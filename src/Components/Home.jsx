63b0a505d465b96f31f46897c462e3886720b38e
Arthaz1245 <sergiomendozap96@gmail.com>
2023-08-09 18:53:59 -0500

    proyect

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
-
+import { GiPositionMarker } from "react-icons/gi";
 const Home = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("London");
   const [beginSearch, setBeginSearch] = useState(false);
-
+  const [temperatureUnit, setTemperatureUnit] = useState("Celsius");
   const [dataWeatherList, setDataWeatherList] = useState(null);
+  const convertToFahrenheit = (celsius) => (celsius * 9) / 5 + 32;
+
   const api = {
     url: "https://api.openweathermap.org/data/2.5/",
     key: "70053b7a76de23bdc7582dddcbe10757",
@@ -22,11 +24,11 @@ const Home = () => {
   ];
   const ICON_URL = "https://api.openweathermap.org/img/w/";
   useEffect(() => {
-    const fetchWeatherDataList = async (city) => {
+    const fetchWeatherDataList = async () => {
       try {
         const response = await axios.get(`${api.url}/forecast`, {
           params: {
-            q: city,
+            q: input,
             appid: api.key,
             units: "metric",
             //cnt: 6, // 5 days + current day
@@ -37,57 +39,133 @@ const Home = () => {
         console.error("Error fetching weather data:", error);
       }
     };
-    fetchWeatherDataList("Bogota");
+    fetchWeatherDataList();
   }, [input, api.url, api.key]);
   const handleButton = () => {
     setBeginSearch(true);
   };
+  const handleUnitChange = () => {
+    setTemperatureUnit((prevUnit) =>
+      prevUnit === "Celsius" ? "Fahrenheit" : "Celsius"
+    );
+  };
+  const handleSearch = () => {
+    setBeginSearch(false);
+  };
+  if (!dataWeatherList) {
+    return (
+      <div className="bg-slate-800 flex justify-center items-center h-screen w-screen text-9xl text-white">
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col w-full h-full md:flex-row  bg-slate-800 ">
-      <div className="md:w-[32%] md:h-[100%]  bg-slate-800 flex flex-col w-[100%] h-[50%] justify-items-stretch ">
-        <div className="w-[80%] h-10 bg-zinc-500 shadow mt-11 ml-11">
-          <div
-            className="text-gray-200 text-base font-medium text-center px-4"
-            onClick={handleButton}
-          >
-            Search for places
+      {beginSearch ? (
+        <div className="md:w-[32%] md:h-[100%]  bg-slate-800 flex flex-col w-[100%] h-[50%] ">
+          <div className="my-20 mx-11 flex flex-row justify-around">
+            <input
+              type="text"
+              placeholder="Search city..."
+              value={input}
+              className="w-[268px] h-12 border border-gray-200 bg-transparent text-white"
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button
+              className="w-[86px] h-12 bg-indigo-600"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          {dataWeatherList?.list[0]?.weather[0] && (
-            <>
-              <img
-                className="w-[202px] h-[234px]"
-                src={
-                  ICON_URL + dataWeatherList?.list[0]?.weather[0].icon + ".png"
-                }
-                alt={dataWeatherList?.list[0]?.weather[0].main}
-              />
-            </>
-          )}
-          <div>
-            <span className="text-gray-200 text-[144px] font-medium">
-              {Math.round(dataWeatherList?.list[0]?.main.temp)}
-            </span>
-            <span className="text-gray-400 text-5xl font-medium">℃</span>
+      ) : (
+        <div className="md:w-[32%] md:h-[100%]  bg-slate-800 flex flex-col w-[100%] h-[50%] justify-items-stretch ">
+          <div className="w-[80%] h-10 bg-zinc-500 shadow mt-11 ml-11">
+            <div
+              className="text-gray-200 text-base font-medium text-center px-4"
+              onClick={handleButton}
+            >
+              Search for places
+            </div>
           </div>
-          <div className="text-center text-gray-400 text-4xl font-semibold">
-            Shower
-          </div>
-          <div className="flex flex-row my-7 justify-around">
-            <div className="text-zinc-400 text-lg font-medium">{`Today • ${
-              dayNames[new Date(dataWeatherList?.list[0].dt * 1000).getDay()]
-            }   ${new Date(
-              dataWeatherList?.list[0].dt * 1000
-            ).getDate()} `}</div>
-          </div>
-          <div className="text-zinc-400 text-lg font-semibold my-7">
-            {dataWeatherList?.city.name}
+          <div className="flex flex-col items-center">
+            {dataWeatherList?.list[0]?.weather[0] && (
+              <>
+                <img
+                  className="w-[202px] h-[234px]"
+                  src={
+                    ICON_URL +
+                    dataWeatherList?.list[0]?.weather[0].icon +
+                    ".png"
+                  }
+                  alt={dataWeatherList?.list[0]?.weather[0].main}
+                />
+              </>
+            )}
+            {temperatureUnit === "Fahrenheit" ? (
+              <div>
+                <span className="text-gray-200 text-[144px] font-medium">
+                  {convertToFahrenheit(
+                    Math.round(dataWeatherList?.list[0]?.main.temp)
+                  )}
+                </span>
+                <span className="text-gray-400 text-5xl font-medium">°F</span>
+              </div>
+            ) : (
+              <div>
+                <span className="text-gray-200 text-[144px] font-medium">
+                  {Math.round(dataWeatherList?.list[0]?.main.temp)}
+                </span>
+                <span className="text-gray-400 text-5xl font-medium">℃</span>
+              </div>
+            )}
+
+            <div className="text-center text-gray-400 text-4xl font-semibold">
+              {dataWeatherList?.list[0]?.weather[0].main}
+            </div>
+            <div className="flex flex-row my-7 justify-around">
+              <div className="text-zinc-400 text-lg font-medium">{`Today • ${
+                dayNames[new Date(dataWeatherList?.list[0].dt * 1000).getDay()]
+              }   ${new Date(
+                dataWeatherList?.list[0].dt * 1000
+              ).getDate()} `}</div>
+            </div>
+            <div className="text-zinc-400 text-lg font-semibold my-7 flex flex-row justify-around">
+              <GiPositionMarker />
+              {dataWeatherList?.city.name}
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="md:w-[68%] md:h-full bg-neutral-900  w-[100%]">
-        {}
+        <div className="flex flex-row mt-11 w-[80%] justify-end">
+          <div
+            className={
+              temperatureUnit === "Celsius"
+                ? `w-10 h-10  bg-gray-200 rounded-[50px] mr-3 p-2`
+                : `w-10 h-10  bg- bg-slate-600 rounded-[50px] mr-3 p-2`
+            }
+            onClick={handleUnitChange}
+          >
+            <div className="w-[18.10px] h-5 text-slate-900 text-lg font-bold">
+              ℃
+            </div>
+          </div>
+          <div
+            className={
+              temperatureUnit === "Fahrenheit"
+                ? `w-10 h-10  bg-gray-200 rounded-[50px] mr-3 p-2`
+                : `w-10 h-10   bg-slate-600 rounded-[50px] mr-3 p-2`
+            }
+            onClick={handleUnitChange}
+          >
+            <div className="w-[16.19px] h-5 text-center text-slate-900 text-lg font-bold">
+              ℉
+            </div>
+          </div>
+        </div>
         <div className="flex  mt-36 mb-[72px] mx-36 md:flex-row  flex-wrap">
           {dataWeatherList?.list
             ?.filter((_, index) => index % 7 === 0 && index !== 0)
@@ -109,14 +187,27 @@ const Home = () => {
                     alt={forecast.weather[0].main}
                   />
                 )}
-                <div className="flex flex-row justify-evenly">
-                  <div className="text-gray-200 text-base font-medium mx-2">
-                    {Math.round(forecast.main.temp_max)}°C
+                {temperatureUnit === "Fahrenheit" ? (
+                  <div className="flex flex-row justify-evenly">
+                    <div className="text-gray-200 text-base font-medium mx-2">
+                      {convertToFahrenheit(Math.round(forecast.main.temp_max))}
+                      °F
+                    </div>
+                    <div className="text-gray-400 text-base font-medium">
+                      {convertToFahrenheit(Math.round(forecast.main.temp_min))}
+                      °F
+                    </div>
                   </div>
-                  <div className="text-gray-400 text-base font-medium">
-                    {Math.round(forecast.main.temp_min)}°C
+                ) : (
+                  <div className="flex flex-row justify-evenly">
+                    <div className="text-gray-200 text-base font-medium mx-2">
+                      {Math.round(forecast.main.temp_max)}°C
+                    </div>
+                    <div className="text-gray-400 text-base font-medium">
+                      {Math.round(forecast.main.temp_min)}°C
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
             ))}
         </div>
@@ -150,7 +241,10 @@ const Home = () => {
               </div>
               <div className="w-[229px] h-2 bg-gray-200 rounded-[80px]">
                 <div
-                  className={`w-[${dataWeatherList?.list[0].main.humidity}%] h-2 bg-yellow-300 rounded-[80px]`}
+                  className={`h-2 bg-yellow-300 rounded-[80px]`}
+                  style={{
+                    width: `${dataWeatherList?.list[0].main.humidity}%`,
+                  }}
                 ></div>
               </div>
             </div>
@@ -163,11 +257,9 @@ const Home = () => {
               </div>
               <div className="text-center">
                 <span className="text-gray-200 text-[64px] font-bold">
-                  6,4{" "}
+                  {dataWeatherList?.list[0].visibility / 1000}
                 </span>
-                <span className="text-gray-200 text-4xl font-medium">
-                  miles
-                </span>
+                <span className="text-gray-200 text-4xl font-medium"> Km</span>
               </div>
             </div>
             <div className="w-[328px] h-[159px] bg-slate-800 pt-5">
